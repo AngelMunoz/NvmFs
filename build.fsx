97dffabadc3407fd6e91e62c0bf2661caf8aaf9f
Angel D. Munoz <scythes.lol@gmail.com>
2024-03-07 21:49:49 -0600

    bump version in build fsx

--- build.fsx
+++ build.fsx
@@ -84,7 +84,7 @@ module Operations =
 
     let buildBinaries (project: string) (runtime: string) =
         let cmd =
-            let framework = "net7.0"
+            let framework = "net8.0"
             let outdir = $"{outDir}/{runtime}"
 
             let flags =
