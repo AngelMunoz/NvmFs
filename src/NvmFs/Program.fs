b430fb04f67b2bfeecad8a73f16066e934eb6feb
Jordan Marr <jmarr@microdesk.com>
2025-06-22 23:16:33 -0400

    Update to beta5

--- src/NvmFs/Program.fs
+++ src/NvmFs/Program.fs
@@ -1,108 +1,53 @@
 module NvmFs.Main
 
 open FSharp.SystemCommandLine
-open System.CommandLine.Invocation
-open System.CommandLine.Help
+open Input
 open NvmFs.Cmd
 
 let installCommand = command "install" {
   description "Installs the specified node version or the latest LTS by default"
-
   inputs(
-    Input.ArgumentMaybe<string>(
-      "version",
-      "Installs the specified node version"
-    ),
-    Input.OptionMaybe<bool>(
-      [ "-l"; "--lts" ],
-      "Ignores version and pulls down the latest LTS version"
-    ),
-    Input.OptionMaybe<bool>(
-      [ "-c"; "--current" ],
-      "Ignores version and pulls down the latest Current version"
-    ),
-    Input.Option<bool>(
-      [ "-d"; "--default" ],
-      false,
-      "Sets the downloaded version as default"
-    )
+    argumentMaybe "version" |> desc "Installs the specified node version",
+    optionMaybe "--lts" |> alias "-l" |> desc "Ignores version and pulls down the latest LTS version",
+    optionMaybe "--current" |> alias "-c" |> desc "Ignores version and pulls down the latest Current version",
+    option "--default" |> alias "-d" |> def false |> desc "Sets the downloaded version as default"
   )
-
-  setHandler Actions.Install
+  setAction Actions.Install
 }
 
 let uninstallCommand = command "uninstall" {
   description "Uninstalls the specified node version"
-
   inputs(
-    Input.ArgumentMaybe<string>(
-      "version",
-      "Installs the specified node version"
-    )
+    argumentMaybe "version" |> desc "Uninstalls the specified node version"
   )
-
-  setHandler Actions.Uninstall
+  setAction Actions.Uninstall
 }
 
 let useCommand = command "use" {
   description "Sets the Node Version"
-
   inputs(
-    Input.ArgumentMaybe<string>(
-      "version",
-      "Installs the specified node version"
-    ),
-    Input.OptionMaybe<bool>(
-      [ "-l"; "--lts" ],
-      "Ignores version and pulls down the latest LTS version"
-    ),
-    Input.OptionMaybe<bool>(
-      [ "-c"; "--current" ],
-      "Ignores version and pulls down the latest Current version"
-    )
+    argumentMaybe "version" |> desc "Sets the specified node version",
+    optionMaybe "--lts" |> alias "-l" |> desc "Ignores version and pulls down the latest LTS version",
+    optionMaybe "--current" |> alias "-c" |> desc "Ignores version and pulls down the latest Current version"
   )
-
-  setHandler Actions.Use
+  setAction Actions.Use
 }
 
 let listCommand = command "list" {
   description "Shows the available node versions"
-
   inputs(
-    Input.OptionMaybe<bool>(
-      [ "-r"; "--remote" ],
-      "Displays the last downloaded version index in the console"
-    ),
-    Input.OptionMaybe<bool>(
-      [ "-u"; "--update" ],
-      "Use together with --remote, pulls the version index from the node website"
-    )
+    optionMaybe "--remote" |> alias "-r" |> desc "Displays the last downloaded version index in the console",
+    optionMaybe "--update" |> alias "-u" |> desc "Use together with --remote, pulls the version index from the node website"
   )
-
-  setHandler Actions.List
-}
-
-let rootHandler(ctx: InvocationContext) = task {
-  let hc =
-    HelpContext(
-      ctx.HelpBuilder,
-      ctx.Parser.Configuration.RootCommand,
-      System.Console.Out
-    )
-
-  ctx.HelpBuilder.Write(hc)
-
-  return 0
+  setAction Actions.List
 }
 
 [<EntryPoint>]
 let main argv =
   rootCommand argv {
-    description
-      "nvmfs is a simple node version manager that just downloads and sets node versions. That's it!"
-
-    inputs(Input.Context())
-    setHandler rootHandler
+    description "nvmfs is a simple node version manager that just downloads and sets node versions. That's it!"
+    inputs context
+    helpActionAsync
     addCommand installCommand
     addCommand uninstallCommand
     addCommand useCommand
